b882fc5bc54b2741d5806bc430faf383e1ca4c56
Taci Shlosberg <tshlosberg@gmail.com>
2020-07-06 17:29:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -88,8 +88,8 @@ def checkout(cart, coupons)
   clearance_items = apply_clearance(cart_with_coupons)
   binding.pry
   
-  clearance_items.collect { |item|
-    
+  clearance_items.each do |item|
+    item[:price][:count].
   end
   total 
 end
